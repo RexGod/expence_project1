597fbc7b3247a3c17cacb40390409cef5b3e36df
shadmehr <shadmehrmanoochehri@gmial.com>
2022-08-06 16:34:56 +0430

    add id featur to transaction class

--- lib/model/transitoin.dart
+++ lib/model/transitoin.dart
@@ -2,6 +2,6 @@ class Transaction {
   final String title;
   final double amount;
   final DateTime date;
-
-  Transaction({required this.title, required this.amount, required this.date});
+  final String id;
+  Transaction({required this.title, required this.amount, required this.date , required this.id});
 }
